628660b4f31dfa2e44d0c4abf38849596900d1f7
billy3321 <billy3321@gmail.com>
2014-10-20 16:18:03 +0800

    finish sidekiq work

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+#Redis.current = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 1)
+#Redis.current = Redis.new("redis://127.0.0.1:6379/1")
+$redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 1)
