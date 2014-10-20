411580194877d348ea11335c232770e2c50a0c1d
billy3321 <billy3321@gmail.com>
2014-10-20 16:21:29 +0800

    fix a bug on delay_for

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
   def update
     #respond_to do |format|
     if @article.update(article_params)
-      @article.delay_for(5.minutes.from_now).set_random_string(@article.content)
+      @article.delay_for(5.minutes).set_random_string(@article.content)
       redirect_to @article, notice: 'Article was successfully updated.'
         #format.json { render :show, status: :ok, location: @article }
     else
