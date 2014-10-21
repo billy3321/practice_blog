7b7569338cf1d1ee3d68ad6fa031ce377ce5851d
billy3321 <billy3321@gmail.com>
2014-10-21 12:12:35 +0800

    remove sidekiq_client

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,6 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Capybara::DSL, :type => :request
   config.include Devise::TestHelpers, type: :controller
-  #config.include SidekiqClient
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
