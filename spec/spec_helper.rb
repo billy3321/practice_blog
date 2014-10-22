51d554fae5bafa6629a39032960cb3771ecc4dfa
billy3321 <billy3321@gmail.com>
2014-10-22 16:02:38 +0800

    remove rspec-sidekiq gem and everything fine

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-require 'sidekiq/testing'
-
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -28,6 +25,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Capybara::DSL, :type => :request
   config.include Devise::TestHelpers, type: :controller
+  config.include SidekiqClient
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
