19f7278769c41965f8739e16d0830a83515f8417
billy3321 <billy3321@gmail.com>
2014-10-23 12:20:23 +0800

    move the sidekiq test from controller to model

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :article do
     sequence(:title)  { |n| "Article #{n}" }
     sequence(:content) { |n| "Article_#{n} Content"}
-    user
-    category
+    user{ FactoryGirl.create(:user) }
+    category{ FactoryGirl.create(:category) }
   end
 end
