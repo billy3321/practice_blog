1eb1a4cf0af9c231891082d4c1177e592ca11155
billy3321 <billy3321@gmail.com>
2014-10-29 12:18:35 +0800

    rails generate controller Items

--- db/migrate/20141029040927_create_items_products.rb
+++ db/migrate/20141029040927_create_items_products.rb
@@ -1,6 +1,8 @@
 class CreateItemsProducts < ActiveRecord::Migration
   def change
-    create_table :items_products do |t|
+    create_table :items_products, id: false do |t|
+      t.belongs_to :item
+      t.belongs_to :product
     end
   end
 end
