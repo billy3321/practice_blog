6424187026bb217317c41493384c35a9157ba03c
billy3321 <billy3321@gmail.com>
2014-10-30 14:37:07 +0800

    finish the datepicker

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,6 +56,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:user_id, :title, :content, :category_id)
+      params.require(:article).permit(:user_id, :title, :content, :category_id, :publish_at)
     end
 end
